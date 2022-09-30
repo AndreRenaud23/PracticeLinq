58a63d44092588324b6c0dd289efa38a5c32be29
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-30 10:53:07 -0500

    First commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+List<string> videoGames = new()
+{
+    "Splatoon 3",
+    "Zelda",
+    "Minecraft",
+    "Fifa 2023",
+    "Rocket League",
+    "League of Legends",
+    "Starcraft II",
+    "Fall Guys",
+    "Jedi Fallen Order",
+    "Cluster Truck",
+    "Super Mario Bros",
+    "Call of Duty",
+    "Fortnite",
+    "Roblox",
+    "Destiny 2",
+    "Rayman's Adventures",
+    "Madden 22",
+    "Rec room",
+    "NBA 2K22",
+    "Super Smash Bros Ultimate",
+    "Super Mario Odyssey",
+    "Overwatch"
+};
+
+videoGames.OrderBy(x => x.Length)
+    .ThenBy(x => x)
+    .ToList()
+    .ForEach(x => Console.WriteLine(x));
